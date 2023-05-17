7c2e8669178e98e42eef21f89bd5f1ae5774d4c3
Goktug Poyraz <goktug.poyraz@consensys.net>
2023-05-17 15:11:59 +0200

    add unit tests

--- ui/components/app/signature-request-original/signature-request-original.container.js
+++ ui/components/app/signature-request-original/signature-request-original.container.js
@@ -87,7 +87,9 @@ function mapDispatchToProps(dispatch) {
         }),
       );
     },
-    resolvePendingApproval: (id) => dispatch(resolvePendingApproval(id)),
+    resolvePendingApproval: (id) => {
+      dispatch(resolvePendingApproval(id));
+    },
     rejectPendingApproval: (id, error) =>
       dispatch(rejectPendingApproval(id, error)),
     cancelAll: (messagesList) => dispatch(cancelMsgs(messagesList)),
